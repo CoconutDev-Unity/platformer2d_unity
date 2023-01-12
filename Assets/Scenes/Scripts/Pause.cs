3b96cdf2a131555556e5d9fe04a67925a021361f
Artem Derevianko <artems.derevianko@gmail.com>
2023-01-12 17:43:18 +0100

    Added pause time stop

--- Assets/Scenes/Scripts/Pause.cs
+++ Assets/Scenes/Scripts/Pause.cs
@@ -7,11 +7,13 @@ public class Pause : MonoBehaviour
     [SerializeField] private GameObject PauseCanvas;
     public void PauseHandler() {
         PauseCanvas.SetActive(true);
+        
         Time.timeScale = 0f;
     }
 
     public void ContinueHandler() {
         PauseCanvas.SetActive(false);
+
         Time.timeScale = 1f;
     }
 }
