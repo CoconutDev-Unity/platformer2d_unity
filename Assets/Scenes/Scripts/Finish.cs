fc36ebadbc9600fb650e64f42187eb565504b1b9
Artem Derevianko <artems.derevianko@gmail.com>
2023-01-15 18:18:50 +0100

    Added "tutorial", fixed enemy healthbar

--- Assets/Scenes/Scripts/Finish.cs
+++ Assets/Scenes/Scripts/Finish.cs
@@ -5,15 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private GameObject MessageUI;
+
     private bool _isActivated = false;
 
     public void Activate() {
         _isActivated = true;
+        MessageUI.SetActive(false);
     }
 
     public void FinishLevel() {
         if (_isActivated) {
             SceneManager.LoadScene(2);
         }
+
+        else if (!_isActivated) {
+        MessageUI.SetActive(true);
+        }
     }
+    
+    
 }
